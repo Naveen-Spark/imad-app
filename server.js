b8f2bd1bc1d1618f545f46bbc6d1058b6db7fac6
Naveen Spark <nvnspark@gmail.com>
2017-08-15 18:38:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,53 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var article1 = {
+    
+    title: 'Article 1 | Positive Boy',
+    heading: 'B cool nd free Always :)',
+    content: 
+           `<p>
+           
+            Positive thinking is a mental and emotional attitude that focuses on the bright side of life and expects positive results.A positive person anticipates happiness, health and success, and believes he or she can overcome any obstacle and difficulty.
+           </p>`
+         };
+
+function Template(data){
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    
+var htmlTemplate= `
+          <!DOCTYPE html>
+            <html>
+            <head>
+                <title>
+                   ${title}
+                </title>
+                <link href="/ui/style.css" rel="stylesheet" />
+                
+                
+            </head>
+            
+            <body>
+                <div class="Can1">
+               
+                    <h1> ${heading} </h1>
+                    
+                    <p> ${content} </p>
+                  
+                    
+                </div>
+                
+                
+            </body>
+            
+            
+            </html>
+       `; 
+       return htmlTemplate;
+}
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -10,7 +57,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article1', function(req,res) {
- res.sendFile(path.join(__dirname, 'ui', 'article1.html'));  
+ res.send(Template(article1));  
 });
 
 app.get('/article2', function(req,res) {
