8d291e08f0a4d72ff2f40472be6dbf8d04a7b821
Naveen Spark <nvnspark@gmail.com>
2017-08-15 18:42:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,46 @@ var article1 = {
             Positive thinking is a mental and emotional attitude that focuses on the bright side of life and expects positive results.A positive person anticipates happiness, health and success, and believes he or she can overcome any obstacle and difficulty.
            </p>`
          };
+         
+ var article2 = {
+    
+    title: 'Article 2 | Positive Tips ',
+    heading: 'B cool nd free Always :)',
+    content: 
+           `
+            <ol>
+         <li>Only use positive words when talking. ...</li>
+         <li>Push out all feelings that aren't positive. ...</li>
+         <li>Use words that evoke strength and success. ...</li>
+         <li> Practice positive affirmation. ...</li>
+         <li>Direct your thoughts. ...</li>
+         <li>Believe you will succeed. ...</li>
+         <li>Analyze what went wrong.</li>
+        </ol>
+           
+           `
+         };        
+         
+var article3 = {
+    title: 'Article 3 | Positive Attitude ',
+    heading: 'B cool nd free Always :)',
+    content: 
+          `
+        <ol>
+         <li>Keep a gratitude journal. ...</li>
+         <li>Reframe your challenges. ...</li>
+         <li>Get good at being rejected. ...</li>
+         <li>Use positive words to describe your life. ...</li>
+         <li>Replace have with get. ...</li>
+         <li>Don't let yourself get dragged into other people's complaints. ...</li>
+         <li>Breathe.</li>
+         <li>Analyze what went wrong.</li>
+        </ol>
+           
+           `
+         };    
+    
+};         
 
 function Template(data){
     var title = data.title;
@@ -61,11 +101,11 @@ app.get('/article1', function(req,res) {
 });
 
 app.get('/article2', function(req,res) {
- res.sendFile(path.join(__dirname, 'ui', 'article2.html'));    
+ res.send(Template(article2));   
 });
 
 app.get('/article3', function(req,res) {
- res.sendFile(path.join(__dirname, 'ui', 'article3.html'));      
+res.send(Template(article3));    
 });
 
 app.get('/ui/style.css', function (req, res) {
